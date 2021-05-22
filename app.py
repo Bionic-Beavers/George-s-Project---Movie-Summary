dd367c429f8b4f60d6150612521c0a5c0adb5329
GVux <62868382+GVux@users.noreply.github.com>
2021-05-22 15:40:33 -0700

    Change the welcome message

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import tkinter as tk
 import requests
 from flask import Flask, request
 
+nltk.download('punkt')
+
 app = Flask(__name__)
 #env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 #app.config.from_object(env_config)
