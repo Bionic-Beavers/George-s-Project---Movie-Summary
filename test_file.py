43d7091f00ac103ab63d08bdcc49dd22ee61a049
GVux <62868382+GVux@users.noreply.github.com>
2021-05-22 17:31:09 -0700

    This file tests to ensure you can interact with my microservice and shows correct syntax

---
+++ test_file.py
@@ -0,0 +1,29 @@
+import requests
+
+
+wiki_article = "hat"
+
+payload = {"article": wiki_article}
+output = requests.get('https://hidden-basin-72940.herokuapp.com/', params=payload)
+print(output.text)
+
+
+# This is if you wanted to test it with multiple articles at once:
+'''test_movies_list = ['Safety Not Guaranteed', 'Titanic (1997 film)', 'The Matrix', 'La Jetée', 'Hot Tub Time Machine', 'WandaVision', 'Lost (TV series)', 'Alias (TV series)', 'computer science', 'curry']
+
+for i in test_movies_list:
+
+
+    payload = {"article": i}
+
+    #output = requests.get('http://127.0.0.1:5000/', params=payload)
+
+    output = requests.get('https://hidden-basin-72940.herokuapp.com/', params=payload)
+
+
+
+    print(output.text)
+    print('############################################################################################################')
+    print('############################################################################################################')
+    print('############################################################################################################')
+    print('############################################################################################################')'''
